972b68d0828c90cbf64db79ac75bf21eca53f838
Fernando <ferchonunez1@hotmail.com>
2020-07-09 12:37:39 -0500

    Meta, Title, Observables

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -16,6 +16,8 @@ import { PagesComponent } from './pages.component';
 //temporal
 import { IncrementadorComponent } from './../components/incrementador/incrementador.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
     declarations: [
@@ -25,7 +27,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         Graficas1Component,
         IncrementadorComponent,
         GraficoDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
 
     ],
     exports: [
